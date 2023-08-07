35c5cf2c07ddac1c67e8aff40eb6b2344ac49ca4
Mujthaba <iammujthaba@gamil.com>
2023-08-07 21:52:51 +0530

    connected backend with frondend

--- index.js
+++ index.js
@@ -8,16 +8,22 @@ const API_URL = "https://api.openweathermap.org/data/2.5/weather?q=malappuram&AP
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-
-try { 
-    const result = await axios.get(API_URL);
-    console.log("it is working");
-    console.log(result.data);
-} catch (error) {
-    console.log("something is wrong....");
+app.get("/", (req, res) => {
     res.render("index.ejs");
-}
+  });
 
+app.post("/get-secret",async (req, res) => {
+    const cityName = req.body;
+    console.log(cityName);
+    try {
+        const response = await axios.get(API_URL);
+        console.log(response.data);
+        res.render("index.ejs");
+
+    } catch (error) {
+        console.error("Failed to make request:", error.message);
+    }    
+});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
